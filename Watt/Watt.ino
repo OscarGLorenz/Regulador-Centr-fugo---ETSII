075c6d894cedea5bec332edeaf3100787e9ad017
Óscar García Lorenz <ogl@gomezdemercado.es>
2018-03-18 16:53:56 +0100

    Soporte por Serial

--- Watt/Watt.ino
+++ Watt/Watt.ino
@@ -10,23 +10,13 @@
 #define REDUCT 46.67
 #define PULSES  32.0
 
-template <typename T>
-union Data {
-  T value;
-  byte buffer[sizeof(T)];
-};
-template <typename T> using Union = Data<T>;
-
 //Control motores
 void setMotor(int16_t pwm) {
   pwm = constrain(pwm, -250, 250);
-  if (pwm < 0)
-  {
+  if (pwm < 0) {
     digitalWrite(5, LOW);
     analogWrite(6, abs(pwm));
-  }
-  else
-  {
+  } else {
     digitalWrite(6, LOW);
     analogWrite(5, abs(pwm));
   }
@@ -38,7 +28,6 @@ volatile double speed = 0;
 void speedRPM() {
   static long lastEncoder = encoder;
   static long lastTime = millis();
-
 
   double incrAngulo = (double) ((long) lastEncoder - (long) encoder ) / 360.0 * PULSES / REDUCT ;
   speed = 1000.0 * incrAngulo / (double) ((millis() - lastTime)) * 60;
@@ -55,7 +44,7 @@ void setup() {
 
   Serial.begin(9600);
   Serial.setTimeout(100);
-
+  
   pinMode(INT_1_PIN, INPUT_PULLUP);
 
   attachInterrupt(digitalPinToInterrupt(INT_1_PIN), 
@@ -75,13 +64,11 @@ void setup() {
 }
 
 void loop() {
-  Union<double> data;
   static long sum = 0;
-  static double ref = -30.0;
+  static double ref = -0.0;
 
   if (Serial.available() > 0) {
-    Serial.readBytes(data.buffer, sizeof(data));
-    ref = -data.value;
+    ref = -Serial.parseFloat();
   } 
 
   double err = ref - speed;
@@ -89,7 +76,7 @@ void loop() {
 
   setMotor(sum * 0.8);
 
-  data.value = -speed;
-  Serial.write(data.buffer, sizeof(data));
+  Serial.println(-speed);
+
   delay(100);
 }
