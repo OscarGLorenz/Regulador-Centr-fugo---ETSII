f3011bfe8e767fad0c4b2620010e7e500d99baf4
Óscar García Lorenz <ogl@gomezdemercado.es>
2018-03-19 11:31:13 +0100

    Limpieza al Watt.ino

--- Watt/Watt.ino
+++ Watt/Watt.ino
@@ -10,6 +10,11 @@
 #define REDUCT 46.67
 #define PULSES  32.0
 
+// Regulador PI
+#define KP 0.0
+#define KI 0.8
+#define WIND_UP 400
+
 //Control motores
 void setMotor(int16_t pwm) {
   pwm = constrain(pwm, -250, 250);
@@ -22,20 +27,28 @@ void setMotor(int16_t pwm) {
   }
 }
 
+// Variables globales para los pulsos del encoder y la velocidad
 volatile long encoder = 0;
 volatile double speed = 0;
 
+// Calcula la velocidad actual
 void speedRPM() {
+  // Variables estáticas, se conservan entre llamadas a la función.
   static long lastEncoder = encoder;
   static long lastTime = millis();
 
+  // El ángulo que ha variado en grados, teniendo en cuenta la reducción.
   double incrAngulo = (double) ((long) lastEncoder - (long) encoder ) / 360.0 * PULSES / REDUCT ;
+
+  // Velocidad en RPM
   speed = 1000.0 * incrAngulo / (double) ((millis() - lastTime)) * 60;
 
+  // Guardar variables para el siguiente ciclo
   lastTime = millis();
   lastEncoder = encoder;
 }
 
+// Rutina de interrupción, cada 100ms se ejecuta speedRPM
 ISR(TIMER0_COMPA_vect) {
   if (!(millis() % 100)) speedRPM();
 }
@@ -47,6 +60,7 @@ void setup() {
   
   pinMode(INT_1_PIN, INPUT_PULLUP);
 
+  // Interrupción del encoder, a flaco de subida, suma 1 al contador encoder.
   attachInterrupt(digitalPinToInterrupt(INT_1_PIN), 
   [&encoder]() {
     encoder++;
@@ -64,18 +78,25 @@ void setup() {
 }
 
 void loop() {
+  // Variables estáticas de la suma de la parte Integral y la referencia.
   static long sum = 0;
   static double ref = -0.0;
 
+  // Actualizar la referencia por serial
   if (Serial.available() > 0) {
     ref = -Serial.parseFloat();
   } 
 
+  // Cálculo del error
   double err = ref - speed;
-  sum = constrain(err+sum,-400,400);
 
-  setMotor(sum * 0.8);
+  // Cálculo de la suma para la I, con anti wind-up.
+  sum = constrain(err+sum,-WIND_UP,WIND_UP);
 
+  // Regulador PI
+  setMotor(KP * err + KI * sum);
+
+  // Mostrar por serial la velocidad
   Serial.println(-speed);
 
   delay(100);
