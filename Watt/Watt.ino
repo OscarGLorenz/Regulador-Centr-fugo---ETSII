9a718cf5eca18ea798c82ab536d3911909870db9
Óscar García Lorenz <ogl@gomezdemercado.es>
2018-03-23 21:33:37 +0100

    ME Encoder Driver

--- Watt/Watt.ino
+++ Watt/Watt.ino
@@ -1,10 +1,11 @@
 #include <Wire.h>
 
 //GPIO configuration
-#define INT_1_PIN     2
+#define INT_1_PIN     3
 #define DIR_1_PIN     10
-#define MOTOR_1_PWM   5
-#define MOTOR_2_PWM   6
+#define MOTOR_1_PWM   6
+#define MOTOR_1_H1    A0
+#define MOTOR_1_H2    A1
 
 //Motor configuration
 #define REDUCT 46.67
@@ -16,14 +17,16 @@
 #define WIND_UP 400
 
 //Control motores
-void setMotor(int16_t pwm) {
+void setMotor(int pwm) {
   pwm = constrain(pwm, -250, 250);
   if (pwm < 0) {
-    digitalWrite(5, LOW);
-    analogWrite(6, abs(pwm));
+    digitalWrite(MOTOR_1_H1, LOW);
+    digitalWrite(MOTOR_1_H2, HIGH);
+    analogWrite(MOTOR_1_PWM, abs(pwm));
   } else {
-    digitalWrite(6, LOW);
-    analogWrite(5, abs(pwm));
+    digitalWrite(MOTOR_1_H1, HIGH);
+    digitalWrite(MOTOR_1_H2, LOW);
+    analogWrite(MOTOR_1_PWM, abs(pwm));
   }
 }
 
@@ -55,18 +58,20 @@ ISR(TIMER0_COMPA_vect) {
 
 void setup() {
 
-  Serial.begin(9600);
-  Serial.setTimeout(100);
-  
+  Serial.begin(115200);
+  Serial.setTimeout(1);
+
   pinMode(INT_1_PIN, INPUT_PULLUP);
 
   // Interrupción del encoder, a flaco de subida, suma 1 al contador encoder.
-  attachInterrupt(digitalPinToInterrupt(INT_1_PIN), 
+  attachInterrupt(digitalPinToInterrupt(INT_1_PIN),
   [&encoder]() {
     encoder++;
-  } 
-  ,RISING);
-  
+  }
+  , RISING);
+  speedRPM();
+  delay(100);
+  speedRPM();
   /*
     Cuando el TIMER0 del ATMEGA328, pase por 0F se ejecutará
     nuestra interrupción (cada ms).
@@ -76,28 +81,26 @@ void setup() {
   OCR0A = 0x0F;
   TIMSK0 |= _BV(OCIE0A);
 }
+double ref = 80.0;
+double sum = 0.0;
 
 void loop() {
-  // Variables estáticas de la suma de la parte Integral y la referencia.
-  static long sum = 0;
-  static double ref = -0.0;
-
-  // Actualizar la referencia por serial
-  if (Serial.available() > 0) {
-    ref = -Serial.parseFloat();
-  } 
-
   // Cálculo del error
-  double err = ref - speed;
+  double err = ref + speed;
 
   // Cálculo de la suma para la I, con anti wind-up.
-  sum = constrain(err+sum,-WIND_UP,WIND_UP);
+  sum  = constrain(err + sum, -WIND_UP, WIND_UP);
 
   // Regulador PI
-  setMotor(KP * err + KI * sum);
+  setMotor(abs(KP * err + KI * sum));
 
-  // Mostrar por serial la velocidad
-  Serial.println(-speed);
+  Serial.print(ref + String(" ")); Serial.println(-speed);
+  delay(100);
 
+  if (Serial.available() > 0) {
+    ref = Serial.parseFloat(); 
+    Serial.parseFloat();
+  }
   delay(100);
 }
+
